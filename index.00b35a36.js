4c06dfc1054785ee1094b7b8b65cd6240a0c81bc
Yura-111 <104792276+Yura-111@users.noreply.github.com>
2022-07-16 13:27:53 +0000

    Deploying to gh-pages from @ Yura-111/ice-cream-project@56676d1505a79a0ea18da00d956a2298813b38f4 🚀

--- index.00b35a36.js
+++
@@ -1,2 +0,0 @@
-!function(){var n={openManuBtn:document.querySelector("[data-manu-open]"),closeManuBtn:document.querySelector("[data-manu-close]"),manu:document.querySelector("[data-manu]")};function e(){n.manu.classList.toggle("is-hidden")}n.openManuBtn.addEventListener("click",e),n.closeManuBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.00b35a36.js.map
