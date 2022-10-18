70f73f77c5a46425506dae2510b5a0dde16be86b
Yura <104792276+Yura-111@users.noreply.github.com>
2022-10-19 00:36:38 +0300

    new

---
+++ dist/index.25139ead.js
@@ -0,0 +1,11 @@
+$(".slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: false,
+    nextArrow: false
+});
+
+//# sourceMappingURL=index.25139ead.js.map
